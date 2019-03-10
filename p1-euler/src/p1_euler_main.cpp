b32f925b7e6ce3100de32d29a7d798b3f977487c
Christian Häge <christian.haege@gmx.net>
2019-03-10 14:31:36 +0100

    added main to Visual studio project for testing TDD

---
+++ p1-euler/src/p1_euler_main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "summator.h"
+
+
+int main(int argc, char** argv)
+{
+	
+	long long sum = 0;
+
+	summator summator;
+
+
+	std::cout << "Solution first problem of ProjectEuler.net: " << sum << std::endl;
+	
+	return 0;
+}
